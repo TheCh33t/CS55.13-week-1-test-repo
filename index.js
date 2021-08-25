d5f5421d12f94e6ca7bd5e4691fbc9212e0a4b50
TheCh33t <67026968+TheCh33t@users.noreply.github.com>
2021-08-25 17:35:21 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+let myhttp = require("http");
+
+let myserver = myhttp.createServer(
+
+
+  function(req, res) {
+console.log(req.url)
+
+let mytext;
+    if ( req.url === "/hey" ) {
+      mytext = "you added hey!";
+    } else {
+      mytext = "you didnt add hey :(";
+    }
+
+    mytext = mytext + " added text";
+
+res.writeHead( 200,  { "Content-Type": "text/plain"});
+
+res.end( mytext + "\n")
+}
+);
+
+myserver.listen(8080, "0.0.0.0")
+
+console.log("server started");
